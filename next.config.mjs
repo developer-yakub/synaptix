35e112659a150332d9382d488bf11126fb5cb5bb
Rayan <app.nest.ry@gmail.com>
2025-10-14 01:24:47 +0530

    final Deployement Changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+    unoptimized: process.env.NODE_ENV === 'development'
+  },
+  distDir: '.next',
+  serverExternalPackages: ['firebase-admin']
+};
 
 export default nextConfig;
